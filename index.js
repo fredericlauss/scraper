3a50d1ca93ec75d1050d05fdf0acd85591816030
frederic <flausson@normandiewebschool.fr>
2022-11-30 18:11:56 +0100

    [ scraper ] fix des link find avec xpath

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const cheerio = require('cheerio');
+const xpath = require("xpath-html");
 var fs = require('fs');
 
-objectsList = [];
+let objectsList = [];
 
 const getPages = async () => {
     for (let i = 1; i < 9; i++) {
@@ -10,51 +10,47 @@ const getPages = async () => {
       }
 };
 
-const getContent = async (link) => {
-  const objectList = [];
+const getProductContent = async (link) => {
+  let objectList = [];
   const response = await fetch(link);
   const body = await response.text();
+  console.log(body);
+//   const $ = cheerio.load(body);
 
-  const $ = cheerio.load(body);
+//   $('.display-4').each((i, title) => {
+//     const titleNode = $(title);
+//     const titleText = titleNode.text();
+//     return titleText;
+//   });
 
-  $('.display-4').each((i, title) => {
-    const titleNode = $(title);
-    const titleText = titleNode.text();
-    objectList.push(titleText);
-  });
+//   $("img").each( function () {
+//     var img = $(this).attr('src');
+//     return img;
+//  });
 
-  $("img").each( function () {
-    var img = $(this).attr('src');
-    objectList.push(img);
-    console.log(objectList);
- });
+//  objectsList.push(objectList);
+//  var json = JSON.stringify(objectsList, null, 4);
+//  console.log(json);
+//  fs.writeFileSync('titleScrap.json', json );
+};
 
-//  var price = $("h3").text();
-//  objectList.push(price);
-  objectsList.push(objectList);
-  var json = JSON.stringify(objectsList);
+const creatJson = async (object) => {
   fs.writeFile('titleScrap.json', json, (err) => {
     if (err)
       console.log(err);
   });
-};
+}
+
 
 const getLinks = async (linkPage) => {
-  // get html text from site
   const response = await fetch(linkPage);
-  // using await to ensure that the promise resolves
   const body = await response.text();
-
-  const $ = cheerio.load(body);
-
-  $('.btn-primary').each( function () {
-    var link = 'http://vps-a47222b1.vps.ovh.net:8484' + $(this).attr('href');
-    getContent(link);
- });
+  const node = xpath.fromPageSource(body).findElement("//a[@class='btn btn-primary'][starts-with(@href, '/product/')]");
+  const href = node.getAttribute("href");
+  const link = "http://vps-a47222b1.vps.ovh.net:8484" + href;
+  getProductContent(link);
 
 };
-
-
 
 getPages();
 
