0f5317283f26dd9f55654d4b21b6feae1164a7af
frederic <flausson@normandiewebschool.fr>
2022-12-04 14:02:39 +0100

    [ scraper ] enregistrement des datas en json

--- index.js
+++ index.js
@@ -1,58 +1,71 @@
 const xpath = require("xpath-html");
 var fs = require('fs');
 
-
-const getPages = async () => {
+const getPages = () => {
+  links = [];
     for (let i = 1; i < 9; i++) {
-        linkstest = 'http://vps-a47222b1.vps.ovh.net:8484/Product/page/' + i;
-        getLinks(linkstest);
+        links.push('http://vps-a47222b1.vps.ovh.net:8484/Product/page/' + i);
       }
+      return links;
 };
+
 const elFetchor = async (pageToFetch) => {
   try {
     const response = await fetch(pageToFetch);
     const body = await response.text();
-    const element = xpath.fromPageSource(body);
-    return element;
+    const document = xpath.fromPageSource(body);
+    return document;
   } catch (error) {
     console.error(error);
   }
 }
 
-const getProductContent = async (link) => {
-  let objectList = [];
-  element = await elFetchor(link);
-  const xpathtitle = element.findElement("//h1");
-  const title = xpathtitle.getText();
-  const xpathdescription = element.findElement("//div[@class='p-1']/p");
-  const description = xpathdescription.getText();
-  const xpathpricedescription = element.findElement("//h3/span");
-  const priceDescription = xpathpricedescription.getText();
-  const xpathprice = element.findElement("//h3");
-  const price = xpathprice.getText();
-  const xpathimg = element.findElement("//div[@class='d-flex']/img");
-  const img = xpathimg.getAttribute("src");
-  console.log(title, description, priceDescription, price, img);
+const getLinks = async () => {
+  const pages = getPages();
+  let allLinks = [];
+  for (const page of pages) {
+    const document = await elFetchor(page);
+    const links = document.findElements("//a[@class='btn btn-primary'][starts-with(@href, '/product/')]");
+    const linkss = links.map(link=>link.getAttribute("href"));
+    allLinks = [...allLinks,...linkss];
+  }
+  return allLinks;
 };
 
-const creatJson = async (object) => {
-  fs.writeFile('titleScrap.json', json, (err) => {
+const getProductContent = async () => {
+  const links = await getLinks();
+  let allContent = [];
+  for (const link of links) {
+    const documentlink = "http://vps-a47222b1.vps.ovh.net:8484" + link;
+    document = await elFetchor(documentlink);
+    const xpathtitle = document.findElement("//h1");
+    const title = xpathtitle.getText();
+    const xpathdescription = document.findElement("//div[@class='p-1']/p");
+    const description = xpathdescription.getText();
+    const xpathpricedescription = document.findElement("//h3/span");
+    const priceDescription = xpathpricedescription.getText();
+    const xpathprice = document.findElement("//h3");
+    const price = xpathprice.getText().trim();
+    const xpathimg = document.findElement("//div[@class='d-flex']/img");
+    const img = xpathimg.getAttribute("src");
+    const object =  {
+      title,
+      description,
+      priceDescription,
+      price,
+      img
+    };
+    allContent.push(object);
+  }
+  return allContent;
+};
+
+const creatJson = async () => {
+  const allContent = await getProductContent();
+  fs.writeFile('data.json', JSON.stringify(allContent), (err) => {
     if (err)
       console.log(err);
   });
 }
 
-
-const getLinks = async (linkPage) => {
-  element = await elFetchor(linkPage);
-  const node = element.findElements("//a[@class='btn btn-primary'][starts-with(@href, '/product/')]");
-  let i = 0;
-  node.forEach(function() { 
-  const href = node[i++].getAttribute("href");
-  const link = "http://vps-a47222b1.vps.ovh.net:8484" + href;
-  getProductContent(link); 
-});
-};
-
-getPages();
-
+creatJson();
