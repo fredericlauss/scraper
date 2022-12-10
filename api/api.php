569cc2910e2f148d4ee6393f849ce0736d339d6a
frederic <flausson@normandiewebschool.fr>
2022-12-10 16:35:36 +0100

    [ api ] debut pagination

--- api/api.php
+++ api/api.php
@@ -7,12 +7,35 @@ function getAllTshirts() {
 }
 
 function getTshirtsByPage($page) {
-    echo "page avec des tshirt";
     $file = '../data.json'; 
     $data = file_get_contents($file); 
     $obj = json_decode($data);
+
+    // nombre de pages totales
     $length = count($obj);
-    echo($length);
+    $nbpages = ceil($length / 16);
+
+    // page d'avant ou d'apres 
+    if($page != 1) {
+        $previousPage = $page - 1;
+    } else {
+        $previousPage = 0;
+    }
+    if($page < $nbpages) {
+        $nextPage = $page + 1;
+    } else {
+        $nextPage = 0;
+    }
+    
+    // données de la page
+    $debut = $page * 16 - 16;
+    $dataPage = array_slice($obj, $debut, 16);
+    $lengthpage = count($dataPage);
+    echo $debut;
+    echo $lengthpage;
+    $dataJson = json_encode($dataPage,JSON_UNESCAPED_UNICODE);
+
+    // mettre tout dans l'array ( données page avant/apres + data)
 }
 
 function getTshirtById($id) {
@@ -21,7 +44,7 @@ function getTshirtById($id) {
     $data = file_get_contents($file); 
     $obj = json_decode($data);
     $dataId = $obj[$idJson];
-    $result = json_encode($dataId);
+    $result = json_encode($dataId,JSON_UNESCAPED_UNICODE);
     sendJSON($result);
     // sendJSON($dataId);
 }
