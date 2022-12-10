8273264d5c9bc660a61b9ee25c18ee63027ea9c2
frederic <flausson@normandiewebschool.fr>
2022-12-10 15:04:22 +0100

    [ api ] init et debut des urls

---
+++ api/api.php
@@ -0,0 +1,13 @@
+<?php
+
+function getAllTshirts() {
+    echo "liste de tout les tshirts";
+}
+
+function getTshirtsByPage($page) {
+    echo "page avec des tshirt";
+}
+
+function getTshirtById($id) {
+    echo "un tshirt par id";
+}
---
+++ api/index.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once("./api.php");
+// page 1 default/tshirts
+// t-shirts par pages tshirts/:nbpage
+// un t-shirt en particulier tshirt/:id
+
+try {
+    if(!empty ($_GET['demande'])) {
+        // séparer l'url quand il y a des "/"
+        $url = explode("/", filter_var($_GET['demande'],FILTER_SANITIZE_URL));
+        // séparation des url pour tshirt ou tshirts
+        switch($url[0]) {
+            case "tshirts" :
+                if(empty($url[1])) {
+                    getAllTshirts();
+                } else {
+                    getTshirtsByPage($url[1]);
+                }
+            break;
+            case "tshirt" : 
+                if(!empty($url[1])) {
+                    getTshirtById($url[1]);
+                } else {
+                    throw new Exception ("problème dans la saisie de l'url, pas de page rensseignée");
+                }
+            break;
+            default : throw new Exception ("problème dans la saisie de l'url, la demande n'est pas valide");
+        }
+    } else {
+        throw new Exception ("pb de récup de Data");
+    }
+} catch(Exception $e) {
+    $erreur =[
+        "message" => $e->getMessage(),
+        "code" => $e->getCode()
+    ];
+    print_r($erreur);
+}
+
