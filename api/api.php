8042373d23d9995d417883d07db5da0f0558eae6
frederic <flausson@normandiewebschool.fr>
2022-12-10 15:39:26 +0100

    [ api ] routing + tout les tshirts et tshirt by id

--- api/api.php
+++ api/api.php
@@ -1,13 +1,33 @@
 <?php
 
 function getAllTshirts() {
-    echo "liste de tout les tshirts";
+    $file = '../data.json'; 
+    $result = file_get_contents($file); 
+    sendJSON($result);
 }
 
 function getTshirtsByPage($page) {
     echo "page avec des tshirt";
+    $file = '../data.json'; 
+    $data = file_get_contents($file); 
+    $obj = json_decode($data);
+    $length = count($obj);
+    echo($length);
 }
 
 function getTshirtById($id) {
-    echo "un tshirt par id";
+    $idJson = $id - 1;
+    $file = '../data.json'; 
+    $data = file_get_contents($file); 
+    $obj = json_decode($data);
+    $dataId = $obj[$idJson];
+    $result = json_encode($dataId);
+    sendJSON($result);
+    // sendJSON($dataId);
+}
+
+function sendJSON($data) {
+    header("Access-Control'Allow-Origin: *");
+    header("Content-Type: application/json");
+    echo ($data);
 }
--- api/index.php
+++ api/index.php
@@ -2,7 +2,7 @@
 
 require_once("./api.php");
 // page 1 default/tshirts
-// t-shirts par pages tshirts/:nbpage
+// t-shirts par pages tshirts/:nbpage ( avec $data, page actuelle, page précédente , page suivante)
 // un t-shirt en particulier tshirt/:id
 
 try {
@@ -22,7 +22,7 @@ try {
                 if(!empty($url[1])) {
                     getTshirtById($url[1]);
                 } else {
-                    throw new Exception ("problème dans la saisie de l'url, pas de page rensseignée");
+                    throw new Exception ("problème dans la saisie de l'url, pas d'id renssegnée");
                 }
             break;
             default : throw new Exception ("problème dans la saisie de l'url, la demande n'est pas valide");
